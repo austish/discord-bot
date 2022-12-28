55ce71c9c0bfb6e372f703fe175f915a3c43e838
Quach, Austin <aquach17@go.pasadena.edu>
2022-12-28 15:00:52 -0800

    user data saved for each server, leaderboard done

--- discordBot.py
+++ discordBot.py
@@ -1,8 +1,12 @@
 import discord
-from functions import rps
-import interactions
+import json
 from discord import app_commands
 from discord.ext import commands
+
+from functions import rps
+from functions import store_user_data
+from functions import get_user_data
+from functions import get_leaderboard
 
 bot = commands.Bot(command_prefix = '!', intents = discord.Intents.all())
 TOKEN = 'MTA0MDg3NzA0MDM1OTk4MTA5Ng.GPmMVh.9C_oN9zDzAxGN9TiqyEnSfXdjOgi3Fnw8PcbpI'
@@ -16,10 +20,12 @@ async def on_ready():           #on startup
     except Exception as e:
         print(e)
 
-#hello comannd
+
+#hello command
 @bot.tree.command(name='hello', description='Say hello to the bot')                             #define bot command
 async def hello(interaction: discord.Interaction):                                              #define python command
     await interaction.response.send_message(f'hello {interaction.user.mention}')                #return python function
+
 
 #rps command
 @bot.tree.command(name='rps', description='Play rock, paper, scissors with the bot')
@@ -33,6 +39,7 @@ async def r_p_s(interaction: discord.Interaction, choice: discord.app_commands.C
     result = f'You selected: `{choice.name}` \n{rps(choice.name.lower())}'
     await interaction.response.send_message(result)                                             
 
+
 #player points command
 @bot.tree.command(name='playerpoint', description='Award/remove player points')
 @app_commands.describe(choice = 'reward or penalize')
@@ -40,32 +47,34 @@ async def r_p_s(interaction: discord.Interaction, choice: discord.app_commands.C
     discord.app_commands.Choice(name = 'Award', value = 0),
     discord.app_commands.Choice(name = 'Remove', value = 1),
 ])
-async def test(interaction: discord.Interaction, member: discord.Member, *, choice: discord.app_commands.Choice[int]):
-    if choice.value == 0:
-        await interaction.response.send_message(f'You awarded {member.mention} a player point!')    
+async def test(interaction: discord.Interaction, *, choice: discord.app_commands.Choice[int], member: discord.Member):
+    if member == interaction.user:
+        await interaction.response.send_message('You can\'t playerpoint yourself bruh')  
+    elif choice.value == 0:
+        store_user_data(interaction.guild.id, member.id, 1)
+        await interaction.response.send_message(f'{interaction.user.mention} awarded {member.mention} a player point!')    
     else:
-        await interaction.response.send_message(f'You removed a player point from {member.mention}')     
+        store_user_data(interaction.guild.id, member.id, -1)
+        await interaction.response.send_message(f'{interaction.user.mention} removed a player point from {member.mention}')     
 
 
+#check player points
+@bot.tree.command(name='check_points', description='check player points')
+async def check_points(interaction: discord.Interaction):
+    points = get_user_data(interaction.guild.id, interaction.user.id)
+    await interaction.response.send_message(f'You have {points} player points.')
 
-# @bot.event
-# async def on_message(message):
-#     #if message author is the bot, do nothing
-#     if message.author == bot.user:
-#         return
 
-#     username = str(message.author).split('#')[0]    #splits username at '#', [0] returns first part of split
-#     user_message = str(message.content)
-#     channel = str(message.channel.name)
-
-#     #embeded message
-#     if user_message[1:] == 'help':
-#         em = discord.Embed(title = 'commands')
-#         em.add_field(name = '!hello', value = 'hello')
-#         em.add_field(name = '!roll', value = 'dice roll')
-#         em.add_field(name = '!rps (choice)', value = 'play rock, paper, scissors with the bot')
-#         await message.channel.send(embed = em)
-#     else: 
-#         await send_message(message, username, user_message[1:])
+#player point leaderboard
+@bot.tree.command(name='leaderboard', description='display playerpoint leaderboard')
+async def leaderboard(interaction: discord.Interaction):
+    em = discord.Embed(title = 'Player Point Leaderboard')
+    count = 0
+    board = get_leaderboard(interaction.guild.id)
+    for i in board:
+        count += 1
+        user = await bot.fetch_user(i)
+        em.add_field(name = (f'{count}. {user.name}'), value = (f'Points: {board[i]}'), inline=False)
+    await interaction.response.send_message(embed = em)
 
 bot.run(TOKEN)
--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
-import random
+import random, json , os
+import numpy as np
 
 def rps(user_input: str) -> str:
     list = ["rock", "paper", "scissors"]
@@ -31,3 +32,50 @@ def rps(user_input: str) -> str:
         return 'invalid input'
     return (f'Bot played {bot_input}. You {result}!')
 
+def store_user_data(server_id, user_id, data):
+    #check if file exists:
+    if os.path.exists(f"C:\\Users\\austi\\OneDrive\\Documents\\CS\\python\\discord bot\\data\\{server_id}.json"):
+        print (f'{server_id}.json exists')
+    else:
+        with open((f'data\\{server_id}.json'), 'w') as f:
+            json.dump({}, f)
+            print (f'{server_id}.json created')
+
+    #read file content
+    with open((f'data\\{server_id}.json'), 'r') as f:
+        user_data = json.load(f)            #store file into dict
+
+    #check if user in json file
+    if str(user_id) not in user_data:   
+        user_data[str(user_id)] = 0
+
+    #update data
+    user_data[str(user_id)] += data 
+
+    #write to json file
+    with open((f'data\\{server_id}.json'), 'w') as f:
+        f.seek(0)
+        json.dump(user_data, f)
+        f.truncate()
+
+def get_user_data(server_id, user_id):
+    #read file content
+    with open((f'data\\{server_id}.json'), 'r') as f:
+        user_data = json.load(f)
+
+    #return data
+    if user_id in user_data:
+        return user_data.get(user_id)
+    else:
+        return 0
+
+def get_leaderboard(server_id):
+    with open((f'data\\{server_id}.json'), 'r') as f:
+        user_data = json.load(f)
+    
+    keys = list(user_data.keys())
+    values = list(user_data.values())
+    sorted_value_index = np.argsort(values)[::-1]
+    sorted_dict = {keys[i]: values[i] for i in sorted_value_index}
+ 
+    return sorted_dict
